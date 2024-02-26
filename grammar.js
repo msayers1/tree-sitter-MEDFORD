dc5d04fa7607999e68750702ae4e73ded645c550
Michael Sayer <msayers1@gmail.com>
2024-02-25 19:11:37 -0500

    Update: Started the grammar and setup another dev environment. Added a sample file.

--- grammar.js
+++ grammar.js
@@ -3,6 +3,26 @@ module.exports = grammar({
   
     rules: {
       // TODO: add the actual grammar rules
-      source_file: $ => 'hello'
+      medford_file: $ => repeat($._definition),
+
+      _definition: $ => choice(
+        $.clause_definition,
+        $.comment_definition,
+        // TODO: other kinds of @@, Macros, 
+      ),
+      clause_definition: $ => seq(
+         '@',
+        $.major_tag,
+        $.parameter_list,
+        $._type,
+        $.block
+      ),
+      comment_definition: $ => seq(
+        '#',
+       $.comment
+      ),
+      identifier: $ => /[A-z]+/,
+
+      number: $ => /\d+/
     }
   });
